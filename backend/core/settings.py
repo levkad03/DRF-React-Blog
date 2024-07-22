f944f68075da8f5ff51c622ccb2c41dbd29841e8
Lev Kadatskyi <levkadatscky@gmail.com>
2024-07-22 10:54:02 +0300

    created category and post model

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "blog",
     "blog_api",
 ]
@@ -88,7 +89,7 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",  # noqa: E501
     },
     {
         "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
