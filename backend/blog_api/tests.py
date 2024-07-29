85e503faf4e7639689c22b54a4f15e3cb5cbc755
Lev Kadatskyi <levkadatscky@gmail.com>
2024-07-29 15:06:20 +0300

    created test for the models and their views

--- backend/blog_api/tests.py
+++ backend/blog_api/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from blog.models import Category, Post
+
+
+class PostTests(APITestCase):
+    def test_view_posts(self):
+        """
+        Ensure we can view all objects.
+        """
+        url = reverse("blog_api:listcreate")
+        response = self.client.get(url, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_account(self):
+        """
+        Ensure we can create a new Post object and view object.
+        """
+        self.test_category = Category.objects.create(name="django")
+
+        self.test_user1 = User.objects.create_user(
+            username="test_user1", password="123456789"
+        )
+        data = {"title": "new", "author": 1, "excerpt": "new", "content": "new"}
+        url = reverse("blog_api:listcreate")
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(len(response.data), 6)
+        root = reverse(("blog_api:detailcreate"), kwargs={"pk": 1})
+        response = self.client.get(root, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
