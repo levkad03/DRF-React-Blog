5972448164ff113f869ba6df48d04de0c2fb32e4
Lev Kadatskyi <levkadatscky@gmail.com>
2024-07-31 15:01:30 +0300

    added cors headers to the django project, added logic to get the data from server to client

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,29 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Posts from './components/Posts';
+import PostLoadingComponent from './components/PostLoading';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const PostLoading = PostLoadingComponent(Posts);
+  const [appState, setAppState] = useState({
+    loading: false,
+    posts: null,
+  });
 
+  useEffect(() => {
+    setAppState({ loading: true });
+    const apiUrl = `http://127.0.0.1:8000/api/`;
+    fetch(apiUrl)
+      .then(data => data.json())
+      .then(posts => {
+        setAppState({ loading: false, posts: posts });
+      });
+  }, [setAppState]);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <div className="App">
+      <h1>Latest Posts</h1>
+      <PostLoading isLoading={appState.loading} posts={appState.posts} />
+    </div>
   );
 }
-
 export default App;
---
+++ frontend/src/components/Posts.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Posts = () => {
+  // TODO: Posts Page
+  return <div>Posts</div>;
+};
+
+export default Posts;
