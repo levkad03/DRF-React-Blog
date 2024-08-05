9684992693c18cb2462bbaa9816ed17092292f52
Lev Kadatskyi <levkadatscky@gmail.com>
2024-08-05 15:19:06 +0300

    added custom user model

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "blog",
     "blog_api",
     "corsheaders",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -136,6 +137,8 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
 }
+
+AUTH_USER_MODEL = "users.NewUser"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
