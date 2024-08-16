a6b07282b9eae73eb20abbcc66e8060bf688a04b
Lev Kadatskyi <levkadatscky@gmail.com>
2024-08-16 15:16:31 +0300

    added api documentation

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -160,3 +160,5 @@ SIMPLE_JWT = {
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {"DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema"}
