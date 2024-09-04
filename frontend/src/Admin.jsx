7565e8b3880acf932c08b1f7eaf6d9e9bafa229b
Lev Kadatskyi <levkadatscky@gmail.com>
2024-09-04 13:32:17 +0300

    added admin component

---
+++ frontend/src/Admin.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import Posts from './components/posts/Posts';
+import axiosInstance from './Axios';
+import PostLoadingComponent from './components/posts/PostLoading';
+
+const Admin = () => {
+  const PostLoading = PostLoadingComponent(Posts);
+  const [appState, setAppState] = useState({
+    posts: [],
+    loading: false,
+  });
+  useEffect(() => {
+    axiosInstance.get().then(res => {
+      const allPosts = res.data;
+      setAppState({ posts: allPosts, loading: false });
+      console.log(res.data);
+    });
+  }, [setAppState]);
+  return (
+    <div className="App">
+      <h1>Latest Posts</h1>
+      <PostLoading isLoading={appState.loading} posts={appState.posts} />
+    </div>
+  );
+};
+
+export default Admin;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,6 +10,7 @@ import Login from './components/auth/Login.jsx';
 import Logout from './components/auth/Logout.jsx';
 import Single from './components/posts/Single.jsx';
 import Search from './components/posts/Search.jsx';
+import Admin from './Admin.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Router>
@@ -22,6 +23,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path="/logout" element={<Logout />} />
         <Route path="/post/:slug" element={<Single />} />
         <Route path="/search" element={<Search />} />
+        <Route path="/admin" element={<Admin />} />
       </Routes>
       <Footer />
     </React.StrictMode>
